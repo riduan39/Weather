fcf5d95654d969e2b1e43871f51748e5be175a4d
riduan <mohammadriduan39@gmail.com>
2021-12-15 14:58:18 +0600

    weather

--- lib/weather.dart
+++ lib/weather.dart
@@ -1,6 +1,13 @@
+import 'dart:convert';
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:http/http.dart' as http;
 
 class weather extends StatefulWidget {
   const weather({Key? key}) : super(key: key);
@@ -10,13 +17,56 @@ class weather extends StatefulWidget {
 }
 
 class _weatherState extends State<weather> {
+  var cityname,
+      maintemp,
+      min,
+      max,
+      humidity,
+      pressure,
+      sunrise,
+      sunset,
+      timezone,
+      country;
+
+  Future getWeather() async {
+    final respone = await http.get(Uri.parse(
+        "https://api.openweathermap.org/data/2.5/weather?q=dhaka&units=metric&appid=3d3d4c2b7454452cbd8e1cb7b6fc56fb"));
+
+    var showData = jsonDecode(respone.body);
+
+    setState(() {
+      this.cityname = showData["name"];
+      this.maintemp = showData["main"]["temp"];
+      this.min = showData["main"]["temp_min"];
+      this.max = showData["main"]["temp_max"];
+      this.humidity = showData["main"]["humidity"];
+      this.pressure = showData["main"]["pressure"];
+      this.sunrise = showData["sys"]["sunrise"];
+      this.sunset = showData["sys"]["sunset"];
+      this.timezone = showData["sys"]["timezone"];
+      this.country = showData["sys"]["country"];
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    this.getWeather();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        backgroundColor: Colors.white70,
         appBar: AppBar(
-          title: Text("Weather App"),
+          title: Text(
+            "Weather",
+            style: TextStyle(fontFamily: "azonix"),
+          ),
           centerTitle: true,
+          backgroundColor: Colors.teal,
         ),
         body: Column(
           children: [
@@ -34,26 +84,43 @@ class _weatherState extends State<weather> {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "You Are On",
-                            style: TextStyle(
-                              fontSize: 30,
+                      SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "You Are On",
+                              style:
+                                  TextStyle(fontSize: 25, fontFamily: "azonix"),
                             ),
-                          ),
-                          Text(
-                            "Cox's Bazar",
-                            style: TextStyle(
-                                fontSize: 35, fontWeight: FontWeight.bold),
-                          ),
-                        ],
+                            SizedBox(width: 10),
+                            Text(
+                              cityname != null
+                                  ? cityname.toString()
+                                  : "Cox's Bazar",
+                              style: TextStyle(
+                                  fontSize: 35, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
                       ),
-                      Text("here is 36\u00B0 C hot"),
-                      SizedBox(
-                        height: 10,
+                      Text(
+                        maintemp != null
+                            ? "here is "+maintemp.toString()+"\u00B0 C"
+                            : "here is 36\u00B0 C hot",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
                       ),
+                      SizedBox(height: 40),
+                      Text(
+                        "Time : 10:22 AM",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: "azonix"),
+                      ),
+                      SizedBox(height: 10),
                     ],
                   ),
                 ),
@@ -62,38 +129,98 @@ class _weatherState extends State<weather> {
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.all(12.0),
-                child: ListView(
-                  children: [
-                    ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text("Minimum Tempreture"),
-                      tileColor: Colors.teal,
-                      trailing: Text("25\u00B0 C"),
+                child: ListView(children: [
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.temperatureLow),
+                    title: Text(
+                      "Minimum Tempreture",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.white),
                     ),
-                    SizedBox(height: 10),
-                    ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text("Minimum Tempreture"),
-                      tileColor: Colors.teal,
-                      trailing: Text("36\u00B0 C"),
+                    tileColor: Colors.teal,
+                    trailing: Text(
+                      min != null ? min.toString() : "20\u00B0 C",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.white),
                     ),
-                    SizedBox(height: 10),
-                    ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text("Minimum Tempreture"),
-                      tileColor: Colors.teal,
-                      trailing: Text("36\u00B0"),
+                  ),
+                  SizedBox(height: 10),
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.temperatureHigh),
+                    title: Text(
+                      "Maximum Tempreture",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.white),
                     ),
-                    SizedBox(height: 10),
-                    ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text("Minimum Tempreture"),
-                      tileColor: Colors.teal,
-                      trailing: Text("36\u00B0"),
+                    tileColor: Colors.teal,
+                    trailing: Text(
+                      max != null ? max.toString() : "36\u00B0 C",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.white),
                     ),
+                  ),
+                  SizedBox(height: 10),
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.thermometerEmpty),
+                    title: Text(
+                      "Pressure",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.white),
+                    ),
+                    tileColor: Colors.teal,
+                    trailing: Text(
+                      pressure != null ? pressure.toString() : "1017",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.phoneAlt),
+                    title: Text(
+                      "Humidity",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.white),
+                    ),
+                    tileColor: Colors.teal,
+                    trailing: Text(
+                      humidity != null ? humidity.toString() : "78",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  ListTile(
+                    leading: Icon(Icons.phone),
+                    title: Text(
+                      "Sunrise",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.white),
+                    ),
+                    tileColor: Colors.teal,
+                    trailing: Text(
+                      sunrise != null ? sunrise.toString() :"2.06",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  ListTile(
+                    leading: Icon(Icons.phone),
+                    title: Text(
+                      "Sunset",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.white),
+                    ),
+                    tileColor: Colors.teal,
+                    trailing: Text(
 
-                  ],
-                ),
+                      sunset != null ? sunset.toString() :"2.06",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.white),
+                    ),
+                  ),
+                ]),
               ),
             ),
           ],
